09ee518067f868c0bdc086fafca4c9b8be7272d2
mell <chanon4817@gmail.com>
2023-05-02 18:41:17 +0700

    feat: hystrix circuit pattern

--- client/main.go
+++ client/main.go
@@ -5,30 +5,52 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/afex/hystrix-go/hystrix"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	
+
 	e.GET("/api", api)
 	e.Logger.Fatal(e.Start(":8001"))
 }
 
+func init() {
+	hystrix.ConfigureCommand("api", hystrix.CommandConfig{
+		Timeout:                500,
+		RequestVolumeThreshold: 1,
+		ErrorPercentThreshold:  100,
+		SleepWindow:            15000,
+	})
+
+	hystrixStreamHandler := hystrix.NewStreamHandler()
+	hystrixStreamHandler.Start()
+	go http.ListenAndServe(":8002", hystrixStreamHandler)
+}
+
 func api(c echo.Context) error {
-	res, err := http.Get("http://localhost:8000/api")
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
+	hystrix.Go("api", func() error {
 
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
+		res, err := http.Get("http://localhost:8000/api")
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
 
-	msg := string(data)
-	fmt.Println(msg)
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
 
-	return c.String(http.StatusOK, msg)
+		msg := string(data)
+		fmt.Println(msg)
+
+		return nil
+	}, func(err error) error {
+		fmt.Println(err)
+		return nil
+	})
+
+	return nil
 }
